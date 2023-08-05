ffea2399fc4e6e1a687f8a2cdc348a5719581367
MohdAmmar01 <mohdammaransari01@gmail.com>
2023-08-05 22:26:44 +0530

    made some changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import styles from "../styles/navbar.module.css"
-import profile from "../public/images/profile.png"
 import Image from 'next/image'
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-     <Image alt="profile_Picture"  className={styles.profile} src={profile}  />
+     <Image alt="profile_Picture"  className={styles.profile} src="/images/profile.png"  />
     </nav>
   )
 }
